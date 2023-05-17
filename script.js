7e0191f14b12664349e0579f77065ac1cc88fed6
@joselmosilva <joselmom@yahoo.com.br>
2023-05-17 15:14:25 -0300

    atualizar

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Ola estou por aqui")
+
+function abrir() {
+    document.querySelector("form").style.opacity = 1
+    document.querySelector("main").style.opacity = 0.2
+
+}
+
+function fechar() {
+    document.querySelector("form").style.opacity = 0
+    document.querySelector("main").style.opacity = 0.1
+}
+
+console.log()
